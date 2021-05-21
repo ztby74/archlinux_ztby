33ffb1a08bc9c4a3bcf9284d1270e6bc9303de39
ztby74 <83415876+ztby74@users.noreply.github.com>
2021-05-21 14:40:06 +0800

    5
    5

---
+++ 5.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "arch" >> /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1       localhost" >> /etc/hosts
+echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
+echo "140.82.113.3  github.com "  >> /etc/hosts
+echo "185.199.108.133  raw.githubusercontent.com" >> /etc/hosts
+echo "185.199.109.133  raw.githubusercontent.com" >> /etc/hosts
