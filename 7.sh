abd982cd39a9c37d36d90372db660d757ab910ca
ztby74 <83415876+ztby74@users.noreply.github.com>
2021-05-24 11:07:18 +0800

    Update 7.sh

--- 7.sh
+++ 7.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pacman -S network manager network-manager-applet nm-connection-editor grub efibootmgr os-prober dosfstools ntfs-3g mtools amd-ucode
+pacman -S networkmanager network-manager-applet nm-connection-editor grub efibootmgr os-prober dosfstools ntfs-3g mtools amd-ucode noto-fonts
