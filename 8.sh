a09ab9af921e284099b2693adb060efd4728cb82
ztby74 <83415876+ztby74@users.noreply.github.com>
2021-05-24 11:09:10 +0800

    Update 8.sh

--- 8.sh
+++ 8.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB 
+
+#grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --no-nvram --removable
 grub-mkconfig -o /boot/grub/grub.cfg
 
 systemctl enable NetworkManager.service
