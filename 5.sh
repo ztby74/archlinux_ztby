51e47ccc2fb4e03197f5fddd8d5fb062b338fbe6
ztby74 <83415876+ztby74@users.noreply.github.com>
2021-05-24 11:06:03 +0800

    Update 5.sh

--- 5.sh
+++ 5.sh
@@ -7,5 +7,9 @@ echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
 echo "140.82.113.3  github.com "  >> /etc/hosts
+echo "140.82.113.4  github.com "  >> /etc/hosts
+echo "140.82.133.4  github.com "  >> /etc/hosts
+echo "140.82.114.4  github.com "  >> /etc/hosts
+echo "140.82.112.4  github.com "  >> /etc/hosts
 echo "185.199.108.133  raw.githubusercontent.com" >> /etc/hosts
 echo "185.199.109.133  raw.githubusercontent.com" >> /etc/hosts
